4d59721de96499ccb21c8d88acc02a48a78f3d97
冬日暖阳 <877197753@qq.com>
2016-12-28 11:56:07 +0800

    Initial Commit of Library
    Signed-off-by: 冬日暖阳 <877197753@qq.com>

---
+++ Example/Pods/Target Support Files/Pods-WDYBaseProject_Tests/Pods-WDYBaseProject_Tests-umbrella.h
@@ -0,0 +1,8 @@
+#ifdef __OBJC__
+#import <UIKit/UIKit.h>
+#endif
+
+
+FOUNDATION_EXPORT double Pods_WDYBaseProject_TestsVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_WDYBaseProject_TestsVersionString[];
+
